95fcdb160a30b71ffc626efdd48b82319b3ff81b
Alexandra <alexvks22@users.noreply.github.com>
2017-09-06 01:13:40 +0300

    Add files via upload

---
+++ create_table_queries_and_select.sql
@@ -0,0 +1,97 @@
+﻿DROP TABLE IF EXISTS pending;
+CREATE TABLE pending
+(
+doctorAMKA bigint NOT NULL,
+username varchar NOT NULL,
+password varchar NOT NULL,
+name varchar NOT NULL,
+surname varchar NOT NULL,
+specialty integer NOT NULL,
+PRIMARY KEY (doctorAMKA)
+);
+
+DROP TABLE IF EXISTS pending_pat;
+CREATE TABLE pending_pat
+(
+patientAMKA bigint NOT NULL,
+userid varchar NOT NULL,
+password varchar NOT NULL,
+name varchar NOT NULL,
+surname varchar NOT NULL,
+gender varchar NOT NULL,
+PRIMARY KEY (patientAMKA)
+);
+
+DROP TABLE IF EXISTS patient;
+CREATE TABLE patient
+(
+patientAMKA bigint NOT NULL,
+userid varchar NOT NULL,
+password varchar NOT NULL,
+name varchar NOT NULL,
+surname varchar NOT NULL,
+gender varchar NOT NULL,
+PRIMARY KEY (patientAMKA)
+);
+
+DROP TABLE IF EXISTS drugs;
+CREATE TABLE drugs
+(
+ID serial NOT NULL,
+name varchar NOT NULL,
+description varchar NOT NULL,
+PRIMARY KEY (ID),
+UNIQUE(ID)
+);
+
+DROP TABLE IF EXISTS departments;
+CREATE TABLE departments(
+ID serial NOT NULL,
+name varchar NOT NULL,
+PRIMARY KEY (ID)
+);
+
+DROP TABLE IF EXISTS medical_folder;
+CREATE TABLE medical_folder(
+ID serial NOT NULL,
+patient bigint NOT NULL,
+cure varchar NOT NULL,
+drug_id serial NOT NULL,
+PRIMARY KEY (ID),
+FOREIGN KEY (drug_id) references drugs(ID),
+FOREIGN KEY (patient) references patient(patientAMKA)
+);
+
+CREATE TABLE admin
+( ID serial NOT NULL,
+username varchar NOT NULL,
+password varchar NOT NULL,
+name varchar NOT NULL,
+surname varchar NOT NULL,
+PRIMARY KEY(username) 
+);
+
+
+CREATE TABLE doctor(
+doctorAMKA bigint NOT NULL,
+username varchar NOT NULL,
+password varchar NOT NULL,
+name varchar NOT NULL,
+surname varchar NOT NULL,
+specialty serial NOT NULL,
+PRIMARY KEY (doctorAMKA),
+FOREIGN KEY (specialty) references departments(ID),
+UNIQUE (doctorAMKA)
+);
+
+
+CREATE TABLE appointments(
+ID serial NOT NULL,
+t timestamp NOT NULL,
+patientAMKA bigint NOT NULL,
+doctorAMKA bigint NOT NULL,
+diagnosis varchar,
+PRIMARY KEY (ID),
+FOREIGN KEY (patientAMKA) references patient(patientAMKA),
+FOREIGN KEY (doctorAMKA) references doctor(doctorAMKA)
+);
