9c8048358766ea2c3f46d9188cfa285122afe3f8
Alexandra <alexvks22@users.noreply.github.com>
2018-05-14 22:14:45 +0300

    Update Admin.java

--- Java_dyn_project/src/ask_1/src/lab_ask1/Admin.java
+++ Java_dyn_project/src/ask_1/src/lab_ask1/Admin.java
@@ -9,27 +9,10 @@ import java.sql.SQLException;
 
 import javax.xml.bind.DatatypeConverter;
 
-public class Admin {
+public class Admin extends Database_Helper{
 	
 	
-	public static Connection establish_connection () {
-	    Connection conn = null;
-        String url = "jdbc:postgresql://localhost:5432/";
-        String dbName = "JavaProject";
-        String driver = "org.postgresql.Driver";
-        String userName = "";
-        String password = "";
-        
-        try {Class.forName(driver).newInstance();
-        conn = DriverManager
-                .getConnection(url + dbName, userName, password);
-        }
-        catch(Exception e)
-        {
-        	System.out.println(e.toString());
-        }
-        return conn;
-	}
+	
 
 	public static void insert_and_delete_from_pending(long AMKA,String insert_sql, String delete_sql) {
 
